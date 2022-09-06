4b6293c6774d8cac2325b12738518b0064e0149c
Joao Vitor da Silva Costa <vitor.silva@labs.dcx>
2022-09-06 11:33:01 -0300

    POR FAVOR TERMINE HOJE JOÃO VITOR

--- src/Insertion.java
+++
@@ -1,8 +0,0 @@
-package src;
-
-public class Insertion {
-    @Override
-    public String toString() {
-        return super.toString();
-    }
-}
---
+++ src/ufpb/algoritmo/Insertion.java
@@ -0,0 +1,9 @@
+package src.ufpb.algoritmo;
+
+import java.util.Comparator;
+
+
+public class Insertion {
+        private  Insertion() {}
+    }
+
